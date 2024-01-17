d0b55476a42f294ecbaddfc16d1c071fbe0c3c8b
Raden Ananta Mahardika <121414143+NotInDream@users.noreply.github.com>
2024-01-17 13:02:35 +0700

    Pengumpulan-Tugas-3

---
+++ Tugas-3/Tugas-3A-Looping/looping.dart
@@ -0,0 +1,57 @@
+// //No. 1 Looping While 
+// void main() {
+//     int i = 0;
+
+//     print("LOOPING PERTAMA");
+//     while (i < 20){
+//         i += 2;
+//         print("${i} - I love coding");
+//     }
+
+//     print("LOOPING KEDUA");
+//     while (i > 0){
+
+//         print("${i} - I will become a mobile developer");
+//         i -= 2;
+        
+//     }
+// }
+
+// //No. 2 Looping menggunakan for
+// void main(){
+//     print("OUTPUT");
+//     for (var i = 1; i <= 20; i++) {
+//       if(i % 2 != 0){
+//         if(i % 3 == 0){
+//             print("${i} - I Love Coding");
+//         } else {
+//             print("${i} - Santai");
+//         }
+//       } else {
+//         print("${i} - Berkualitas");
+//       }
+//     }
+// }
+
+// //No. 3 Membuat Persegi Panjang #
+// void main(){
+//     String sx = "";
+//     for (var x = 0; x < 8; x++) {
+//       sx += "#";
+//     }
+//     for (var y = 0; y < 4; y++) {
+//       print(sx);
+//     }
+// }
+
+// //No. 4 Membuat Tangga 
+// void main(){
+//     String stairs = "";
+//     int i = 0;
+//     while (i < 7){
+//         stairs += "#";
+//         print(stairs);
+//         i++;
+//     }
+
+// }
---
+++ Tugas-3/Tugas-3B-Function/function.dart
@@ -0,0 +1,57 @@
+// //No. 1 
+// void main(){
+//   print(teriak());
+// }
+
+// teriak(){
+//   return "Halo Sanbers!";
+// }
+
+// //No. 2 
+// void main(){
+//   var num1 = 12;
+//   var num2 = 4;
+
+//   var hasilKali = kalikan(num1, num2);
+//   print(hasilKali);
+// }
+
+// kalikan(int x, int y){
+//   return (x * y);
+// }
+
+// //No. 3 
+// void main(){
+//   final String name = "Raden Ananta Mahardika";
+//   final int age = 20;
+//   final String address = "Jln. in aja dulu, Sukakamu";
+//   final String hobby = "Gaming";
+ 
+//   final perkenalan = introduce(name, age, address, hobby);
+//   print(perkenalan);
+// }
+
+// introduce(String name, int age, String address, String hobby){
+//   String kenalan = "Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}!";
+//   return kenalan;
+// }
+
+// //No. 4
+// void main(){
+//   print("Faktorial 0 = ${faktorial(0)}");
+//   print("Faktorial -2 = ${faktorial(-2)}");
+//   print("Faktorial 7 = ${faktorial(7)}");
+// }
+
+// faktorial(int x){
+//   if (x <= 0){
+//     return 1;
+//   } else {
+//     int result = 1;
+//     while (x >= 1){
+//       result = result * x;
+//       x -= 1;
+//     }
+//     return result;
+//   }
+// }
