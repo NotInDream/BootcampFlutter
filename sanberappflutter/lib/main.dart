346b76f6d0d7e26a7bfcb2e3fa46a1bf85eac41c
unknown <radenananta.ram@gmail.com>
2024-02-02 17:34:28 +0700

    Pengumpulan Tugas 15

--- sanberappflutter/lib/main.dart
+++ sanberappflutter/lib/main.dart
@@ -1,21 +1,29 @@
-import 'package:flutter/material.dart';
-import 'package:sanberappflutter/Tugas/Tugas13/login_screen.dart';
+import 'package:sanberappflutter/Latihan/Latihan2/pages/page_1.dart';
+import 'package:sanberappflutter/Latihan/Latihan2/routes/page_routes.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Flutter Demo",
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginScreen(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PageOne(),
+      getPages: pageRouteApp.pages,
     );
   }
 }
